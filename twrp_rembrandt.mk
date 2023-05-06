4ee30e71519d72df40094145c663e08693e3f699
mominqimiang <50542889+mominqimiang@users.noreply.github.com>
2023-05-06 16:50:13 +0800

    Update twrp_rembrandt.mk

--- twrp_rembrandt.mk
+++ twrp_rembrandt.mk
@@ -30,7 +30,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device specific configs
-$(call inherit-product, device/xiaomi/rubens/device.mk)
+$(call inherit-product, device/xiaomi/rembrandt/device.mk)
 
 # Device identifier
 PRODUCT_DEVICE := rembrandt
